7248d956527fb798cf20b90eb9b8aa07ebe1ecd7
Gary <yguan15@mylangara.ca>
2023-02-10 12:36:48 +0900

    gary_new_code
    rewrite Grantley's code

--- logic.js
+++ logic.js
@@ -1,3 +1,31 @@
+// part of the code is contributed from: 
+// https://sebhastian.com/javascript-csv-to-array/
+
+const myfile = document.getElementById("csv");
+const myform = document.getElementById("form");
+let contentStr = "", splited = "";
+
+
+myform.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const input = myfile.files[0];
+    const reader = new FileReader();
+
+
+    reader.readAsText(input);
+
+    reader.onload = function (event) {
+        content = reader.result;
+    };
+
+    splited = content.split(/,| /);
+
+    document.getElementById("output").innerHTML = splited;
+});
+
+
+
 function totalPopulation(everyPopulationCellInCsvFile)
 {
     let totalPopulation;
@@ -14,7 +42,7 @@ function avePopPerRepCalc(sumOfPopulations)
 
 function minNumberOfRepsForEachStateDouble(individualStatePopulation, averagePopulationPerRepresentative)
 {
-    let minNumberOfRepsForEachStateDouble = individualStatePopulation/averagePopulationPerRepresentative;
+    let minNumberOfRepsForEachStateDouble = individualStatePopulation / averagePopulationPerRepresentative;
     return minNumberOfRepsForEachStateDouble;
 }
 
